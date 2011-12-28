3cbc685c7ea88512f23127848c943f8e37b68400
Fred Drake <fdrake@acm.org>
2011-12-28 20:42:42 +0000

    release zc.recipe.filetemplate 0.1.0

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ script = zc.recipe.filetemplate:ScriptTemplate
 
 setup(
     name='zc.recipe.filetemplate',
-    version='0',
+    version='0.1.0',
     author='Zvezdan Petkovic',
     author_email='zvezdan@zope.com',
     description=__doc__,
